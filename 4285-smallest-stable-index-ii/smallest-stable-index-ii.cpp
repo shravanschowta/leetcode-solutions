d7f1450f7353badeb6287251f2406512ec76cda4
shravanschowta <shravanschowta100@gmail.com>
2026-09-05 22:53:57 +0530

    Time: 0 ms (100.00%) | Memory: 202.5 MB (85.02%) - LeetSync

---
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> minSuffix(n);
+        minSuffix[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; --i) {
+            minSuffix[i] = min(minSuffix[i + 1], nums[i]);
+        }
+        int maxPrefix = 0;
+        for (int i = 0; i < n; ++i) {
+            maxPrefix = max(maxPrefix, nums[i]);
+            if (maxPrefix - minSuffix[i] <= k) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+};
