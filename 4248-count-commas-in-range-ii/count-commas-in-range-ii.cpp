1ae08bb3785e9d88cb1ace8408e56e7915dbbb3e
shravanschowta <shravanschowta100@gmail.com>
2026-09-09 19:06:31 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (57.33%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -3,12 +3,10 @@ public:
     long long countCommas(long long n) {
         long long totalCommas = 0;
         long long threshold = 1000;
-
         while (n >= threshold) {
             totalCommas += (n - threshold + 1);
             threshold *= 1000;
         }
-
         return totalCommas;
     }
 };
