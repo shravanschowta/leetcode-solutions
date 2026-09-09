1ddf87167e80671d92b9b8a8bf5bee97711549fc
shravanschowta <shravanschowta100@gmail.com>
2026-09-09 19:23:00 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (19.20%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     long long countCommas(long long n) {
-        long long totalCommas = 0;
-        long long threshold = 1000;
-        while (n >= threshold) {
-            totalCommas += (n - threshold + 1);
-            threshold *= 1000;
+        long long totalCommas=0;
+        long long threshold=1000;
+        while(n>=threshold){
+            totalCommas+=(n-threshold+1);
+            threshold*=1000;
         }
         return totalCommas;
     }
